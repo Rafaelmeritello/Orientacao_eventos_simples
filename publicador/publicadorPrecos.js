325b5dd26ff6de9f453e2183d2cf80b9f6cba842
rafael <rafaelmeritello@gmail.com>
2026-05-20 00:08:53 +0200

    tentativa de ajuste de tempo
     ajustado intervalo
    
     ajustado intervalo
    
    ajuste funcao de inicialização de canal
    
    ajuste funcao inicio

--- publicador/publicadorPrecos.js
+++ publicador/publicadorPrecos.js
@@ -1,27 +1,45 @@
 console.log('Publicador preços sendo executado')
 
 const amqp = require('amqplib');
-async function publicar() {
-    const rabbitUrl = 'amqp://user:password@rabbitmq:5672';
-    try{
-        const conexao = await amqp.connect(rabbitUrl)
-        const canal = await conexao.createChannel()
-        const nome_da_fila = 'precos_produtos'
-        const nomeFila = 'precos_produtos';
-        await canal.assertQueue(nomeFila, { durable: true });
-        const dadosPreco = {
-            produto: 'teclado',
-            preco: 299.90,
-        };
-        const mensagem = JSON.stringify(dadosPreco);
-        canal.sendToQueue(nomeFila, Buffer.from(mensagem), { persistent: true });
-        console.log("Conexão fechada com sucesso.");
-    }catch(erro){
-console.error("Erro ao publicar mensagem:", erro);
+const rabbitUrl = 'amqp://user:password@rabbitmq:5672';
+
+let canalGlobal = null;
+let conexaoGlobal = null;
+async function iniciarRabbitMQ() {
+    try {
+        conexaoGlobal = await amqp.connect(rabbitUrl);
+        canalGlobal = await conexaoGlobal.createChannel();
+        await canalGlobal.assertQueue('fila_precos', { durable: true });
+        console.log("Conexão global com RabbitMQ estabelecida com sucesso!");
+    } catch (erro) {
+        console.error("Falha ao iniciar conexão com RabbitMQ:", erro.message);
+       
+        setTimeout(iniciarRabbitMQ, 5000);
     }
 }
-setInterval(() => {
-    publicar();
-}, 3000);
+async function publicar() {
+ if (!canalGlobal) {
+        console.log("Canal ainda não está pronto. Pulando");
+        return;
+    }
 
+    try {
+        const dadosPreco = {
+            produto: 'Teclado',
+            preco: 299.90,
+            
+        };
+        const mensagem = JSON.stringify(dadosPreco);
+
+     
+        canalGlobal.sendToQueue('fila_precos', Buffer.from(mensagem), { persistent: true });
+        console.log(`Enviado com sucesso: ${mensagem}`);
+    } catch (erro) {
+        console.error("Erro ao enviar:", erro.message);
+    }
+}
+iniciarRabbitMQ().then(() => {
+  
+    setInterval(publicar, 5000);
+});
 setInterval(() => {}, 1000 * 60 * 60);
