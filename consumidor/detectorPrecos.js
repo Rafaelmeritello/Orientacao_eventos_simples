e7f8595312b78fe0c49bbbc57677c59e00fa84a5
rafael <rafaelmeritello@gmail.com>
2026-05-19 23:02:52 +0200

    estrutura inicial
    testando publicacao
    
    ajustado nome função
    
    ajuste de dependencia

--- consumidor/detectorPrecos.js
+++ consumidor/detectorPrecos.js
@@ -1,1 +1,2 @@
 console.log('Detector preços sendo executado')
+setInterval(() => {}, 1000 * 60 * 60);
--- publicador/publicadorPrecos.js
+++ publicador/publicadorPrecos.js
@@ -1,1 +1,27 @@
 console.log('Publicador preços sendo executado')
+
+const amqp = require('amqplib');
+async function publicar() {
+    const rabbitUrl = 'amqp://user:password@rabbitmq:5672';
+    try{
+        const conexao = await amqp.connect(rabbitUrl)
+        const canal = await conexao.createChannel()
+        const nome_da_fila = 'precos_produtos'
+        const nomeFila = 'precos_produtos';
+        await canal.assertQueue(nomeFila, { durable: true });
+        const dadosPreco = {
+            produto: 'teclado',
+            preco: 299.90,
+        };
+        const mensagem = JSON.stringify(dadosPreco);
+        canal.sendToQueue(nomeFila, Buffer.from(mensagem), { persistent: true });
+        console.log("Conexão fechada com sucesso.");
+    }catch{
+console.error("Erro ao publicar mensagem:", erro);
+    }
+}
+setInterval(() => {
+    publicar();
+}, 3000);
+
+setInterval(() => {}, 1000 * 60 * 60);
